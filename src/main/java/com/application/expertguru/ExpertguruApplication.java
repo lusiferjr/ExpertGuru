a119310b3edba8c2d6469e54cbd20523fa6404a4
naman <naman160234@gmail.com>
2022-01-10 18:57:52 +0530

    initial commit

---
+++ src/main/java/com/application/expertguru/ExpertguruApplication.java
@@ -0,0 +1,13 @@
+package com.application.expertguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertguruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertguruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/application/expertguru/ExpertguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.expertguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
